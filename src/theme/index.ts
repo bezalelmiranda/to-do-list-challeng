4738ec23d2669f55c3e5d795a12138fb351a454c
Bezalel Miranda <begmr206@gmail.com>
2023-05-29 17:20:42 -0300

    First commit to-do-list-challeng

---
+++ src/theme/index.ts
@@ -0,0 +1,31 @@
+export default {
+    COLORS: {
+        //Colors
+        BLUE_DARK: '#1E6F9F',
+        BLUE: '#4EA8DE',
+        PURPLE_DARK: '#5E60CE',
+        PURPLE: '#8284FA',
+        //Base
+        GRAY_700: '#0D0D0D',
+        GRAY_600: '#1A1A1A',
+        GRAY_500: '#262626',
+        GRAY_400: '#333333',
+        GRAY_300: '#808080',
+        GRAY_200: '#D9D9D9',
+        GRAY_100: '#F2F2F2',
+        //Feedback
+        DANGER: '#E25858',
+    },
+    FONT_FAMILY: {
+        REGULAR: 'Inter_400Regular',
+        BOLD: 'Inter_700Bold',
+    },
+    LINE_HEIGHT: {
+        MD: '140%'
+    },
+    FONT_SIZE: {
+        XS: 12,
+        SM: 14,
+        MD: 16,
+    }
+}
